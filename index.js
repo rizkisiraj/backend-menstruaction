a84120c01e19a43255e73bcffa096efe48e54e8f
rizkisiraj <jomirwa@gmail.com>
2023-03-16 15:14:23 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const cors = require('cors')
+const port = 5000;
+
+app.use(express.json());
+app.use(cors('https://menstruaction-b5dd1.web.app/'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/', (req, res) => {
+    const midtransClient = require('midtrans-client');
+    const SERVER_KEY = 'SB-Mid-server-uOc21Fq7lkWdBObS_PkRiWSj';
+
+// Create Snap API instance
+let snap = new midtransClient.Snap({
+        // Set to true if you want Production Environment (accept real transaction).
+        isProduction : false,
+        serverKey : SERVER_KEY
+    });
+
+    snap.createTransaction(req.body)
+     .then((transaction)=>{
+        // transaction token
+        res.status(200).json(transaction);
+    }).catch(e => {
+        res.status(400).json(e.message);
+    });
+});
+
+app.listen(port, () => {
+    console.log('Express is Running')
+})
