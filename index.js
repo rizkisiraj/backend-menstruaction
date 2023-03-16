16f3967ef31fa87cb86c5e116c6bb791c1238c66
rizkisiraj <jomirwa@gmail.com>
2023-03-16 17:32:35 +0700

    test commit1

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express();
-
-
+const cors = require('cors');
 const port = process.env.PORT || 5002
 
 
@@ -9,6 +8,7 @@ const port = process.env.PORT || 5002
 // app.use(cors())
 
 //middleware
+app.use(cors());
 app.use(express.json())
 // app.disable('view cache');
 
