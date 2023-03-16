2fd8c73fa1616e7cf9f215766dc3d2e804879927
rizkisiraj <jomirwa@gmail.com>
2023-03-16 17:05:39 +0700

    sale

--- index.js
+++ index.js
@@ -1,37 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require("express");
+var app = express();
 
-const app = express();
-const cors = require('cors')
-
-app.use(express.json());
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// app.post('/', (req, res) => {
-//     const midtransClient = require('midtrans-client');
-//     const SERVER_KEY = 'SB-Mid-server-uOc21Fq7lkWdBObS_PkRiWSj';
-
-// // Create Snap API instance
-// let snap = new midtransClient.Snap({
-//         // Set to true if you want Production Environment (accept real transaction).
-//         isProduction : false,
-//         serverKey : SERVER_KEY
-//     });
-
-//     snap.createTransaction(req.body)
-//      .then((transaction)=>{
-//         // transaction token
-//         res.status(200).json(transaction);
-//     }).catch(e => {
-//         res.status(400).json(e.message);
-//     });
-// });
-
-app.get('/', (req,res) => {
-  res.status(200).json('already showing');
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
 });
 
-app.listen(5000, () => {
-  console.log('running start')
+app.get('/', (req,res) => {
+  res.status(200).json("already running");
 })
