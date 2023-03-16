f7c303bf1892b2ac96a4e413ca86b3aa21d870c8
rizkisiraj <jomirwa@gmail.com>
2023-03-16 17:19:58 +0700

    test commit

--- index.js
+++ index.js
@@ -15,7 +15,27 @@ app.use(express.json())
 //router
 app.get("/", (req,res)=>{
     res.status(200).json('already gettin');
-})
+});
+
+app.post('/', (req, res) => {
+    const midtransClient = require('midtrans-client');
+    const SERVER_KEY = 'SB-Mid-server-uOc21Fq7lkWdBObS_PkRiWSj';
+
+// Create Snap API instance
+let snap = new midtransClient.Snap({
+        // Set to true if you want Production Environment (accept real transaction).
+        isProduction : false,
+        serverKey : SERVER_KEY
+    });
+
+    snap.createTransaction(req.body)
+     .then((transaction)=>{
+        // transaction token
+        res.status(200).json(transaction);
+    }).catch(e => {
+        res.status(400).json(e.message);
+    });
+});
 
 
 app.listen(port, ()=>{
