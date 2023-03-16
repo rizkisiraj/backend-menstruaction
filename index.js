d71f74a1daeaa381c915074de985f21a8188ceec
rizkisiraj <jomirwa@gmail.com>
2023-03-16 16:47:17 +0700

    sale

--- index.js
+++ index.js
@@ -8,26 +8,30 @@ app.use(express.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
-    const midtransClient = require('midtrans-client');
-    const SERVER_KEY = 'SB-Mid-server-uOc21Fq7lkWdBObS_PkRiWSj';
+// app.post('/', (req, res) => {
+//     const midtransClient = require('midtrans-client');
+//     const SERVER_KEY = 'SB-Mid-server-uOc21Fq7lkWdBObS_PkRiWSj';
 
-// Create Snap API instance
-let snap = new midtransClient.Snap({
-        // Set to true if you want Production Environment (accept real transaction).
-        isProduction : false,
-        serverKey : SERVER_KEY
-    });
+// // Create Snap API instance
+// let snap = new midtransClient.Snap({
+//         // Set to true if you want Production Environment (accept real transaction).
+//         isProduction : false,
+//         serverKey : SERVER_KEY
+//     });
 
-    snap.createTransaction(req.body)
-     .then((transaction)=>{
-        // transaction token
-        res.status(200).json(transaction);
-    }).catch(e => {
-        res.status(400).json(e.message);
-    });
-});
+//     snap.createTransaction(req.body)
+//      .then((transaction)=>{
+//         // transaction token
+//         res.status(200).json(transaction);
+//     }).catch(e => {
+//         res.status(400).json(e.message);
+//     });
+// });
 
 app.get('/', (req,res) => {
   res.status(200).json('already showing');
 });
+
+app.listen(5000, () => {
+  console.log('running start')
+})
